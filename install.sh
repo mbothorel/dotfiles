542757e1146155d846a438c9a74b3b273b8d3879
Maxence Bothorel <maxence@bothorel.net>
2016-04-17 15:48:49 +0200

    Ajout recursivité

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "Installing dotfiles"
 cp ~/git/dotfiles/bashrc ~/.bashrc
 cp ~/git/dotfiles/zshrc ~/.zshrc
 cp ~/git/dotfiles/vimrc ~/.vimrc
-cp ~/git/dotfiles/vim ~/.vim
-cp ~/git/dotfiles/i3 ~/.config/i3
-cp ~/git/dotfiles/i3status ~/.config/i3status
+cp -R ~/git/dotfiles/vim ~/.vim
+cp -R ~/git/dotfiles/i3 ~/.config/i3
+cp -R ~/git/dotfiles/i3status ~/.config/i3status
 cp ~/git/dotfiles/Xresources ~/.Xresources
 echo "dotfiles installed"
