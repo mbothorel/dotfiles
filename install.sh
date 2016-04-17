12029f395748d017570992206f2029706a0e2137
Maxence Bothorel <maxence@bothorel.net>
2016-04-17 15:52:51 +0200

    Fix .vim

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing dotfiles"
 cp ~/git/dotfiles/bashrc ~/.bashrc
 cp ~/git/dotfiles/zshrc ~/.zshrc
 cp ~/git/dotfiles/vimrc ~/.vimrc
-cp -R ~/git/dotfiles/vim ~/
+cp -R ~/git/dotfiles/vim ~/.vim
 cp -R ~/git/dotfiles/i3 ~/.config/
 cp -R ~/git/dotfiles/i3status ~/.config/
 cp ~/git/dotfiles/Xresources ~/.Xresources
