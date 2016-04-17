abe9109aa81cddf73a5fd28df9b3b40f3a22e88b
Maxence Bothorel <maxence@bothorel.net>
2016-04-17 15:47:28 +0200

    cp au lieu de ln

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Installing symlinks"
-ln -s ~/git/dotfiles/bashrc ~/.bashrc
-ln -s ~/git/dotfiles/zshrc ~/.zshrc
-ln -s ~/git/dotfiles/vimrc ~/.vimrc
-ln -s ~/git/dotfiles/vim ~/.vim
-ln -s ~/git/dotfiles/i3 ~/.config/i3
-ln -s ~/git/dotfiles/i3status ~/.config/i3status
-ln -s ~/git/dotfiles/Xresources ~/.Xresources
-echo "Symlinks installed"
+echo "Installing dotfiles"
+cp ~/git/dotfiles/bashrc ~/.bashrc
+cp ~/git/dotfiles/zshrc ~/.zshrc
+cp ~/git/dotfiles/vimrc ~/.vimrc
+cp ~/git/dotfiles/vim ~/.vim
+cp ~/git/dotfiles/i3 ~/.config/i3
+cp ~/git/dotfiles/i3status ~/.config/i3status
+cp ~/git/dotfiles/Xresources ~/.Xresources
+echo "dotfiles installed"
