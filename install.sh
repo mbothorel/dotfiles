6637bf0dbd3ee214740e4664b5b4598894aaf0b5
Maxence Bothorel <maxence@bothorel.net>
2016-08-15 18:46:05 +0200

    Fix vim plugins installation by download vundle directly in ~/.vim/ instead on git folder

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo "Installing dotfiles"
 cp ~/git/dotfiles/bashrc ~/.bashrc
 cp ~/git/dotfiles/zshrc ~/.zshrc
 cp ~/git/dotfiles/vimrc ~/.vimrc
-cp -R ~/git/dotfiles/vim ~/.vim
+mkdir ~/.vim/bundle -p
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -R ~/git/dotfiles/i3 ~/.config/
 cp -R ~/git/dotfiles/i3status ~/.config/
 cp ~/git/dotfiles/Xresources ~/.Xresources
